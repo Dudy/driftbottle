a4a419be83cb64cd93628879a6230e26df88c1f5
Dirk Podolak <dirk.podolak@essquare.de>
2020-10-05 08:59:50 +0200

    startup, load messages and show them

--- src/main/java/de/pomis/driftbottle/persistence/UserEntity.java
+++ src/main/java/de/pomis/driftbottle/persistence/UserEntity.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user", schema = "schema1")
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public UserEntity() {
